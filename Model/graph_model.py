3c7f68a59258ab813ae8828ac9c345dcf2069b4f
adritad <98999592+adrita78@users.noreply.github.com>
2024-10-21 14:51:26 -0400

    Update graph_model.py

--- Model/graph_model.py
+++ Model/graph_model.py
@@ -38,42 +38,6 @@ from torch_geometric.utils import degree, sort_edge_index
 import torch_sparse
 #import torch_geometric
 
-def permute_within_batch(x, batch):
-    # Enumerate over unique batch indices
-    unique_batches = torch.unique(batch)
-
-    # Initialize list to store permuted indices
-    permuted_indices = []
-
-    for batch_index in unique_batches:
-        # Extract indices for the current batch
-        indices_in_batch = (batch == batch_index).nonzero().squeeze()
-
-        # Permute indices within the current batch
-        permuted_indices_in_batch = indices_in_batch[torch.randperm(len(indices_in_batch))]
-
-        # Append permuted indices to the list
-        permuted_indices.append(permuted_indices_in_batch)
-
-    # Concatenate permuted indices into a single tensor
-    permuted_indices = torch.cat(permuted_indices)
-
-    return permuted_indices
-
-# Example usage
-batch = torch.tensor([0, 0, 0, 1, 1, 1, 1])
-x = torch.tensor([0, 10, 20, 30, 40, 50, 60])
-
-# Get permuted indices
-permuted_indices = permute_within_batch(x, batch)
-
-# Use permuted indices to get the permuted tensor
-permuted_x = x[permuted_indices]
-
-print("Original x:", x)
-print("Permuted x:", permuted_x)
-print("Permuted indices:", permuted_indices)
-
 class GPSConv(torch.nn.Module):
 
     def __init__(
@@ -329,8 +293,43 @@ class GraphModel(torch.nn.Module):
         x = global_add_pool(x, batch)
         # node features are pooled at the graph level using global pooling
         return self.mlp(x)
+        
 
+def permute_within_batch(x, batch):
+    # Enumerate over unique batch indices
+    unique_batches = torch.unique(batch)
 
+    # Initialize list to store permuted indices
+    permuted_indices = []
+
+    for batch_index in unique_batches:
+        # Extract indices for the current batch
+        indices_in_batch = (batch == batch_index).nonzero().squeeze()
+
+        # Permute indices within the current batch
+        permuted_indices_in_batch = indices_in_batch[torch.randperm(len(indices_in_batch))]
+
+        # Append permuted indices to the list
+        permuted_indices.append(permuted_indices_in_batch)
+
+    # Concatenate permuted indices into a single tensor
+    permuted_indices = torch.cat(permuted_indices)
+
+    return permuted_indices
+
+# Example usage
+batch = torch.tensor([0, 0, 0, 1, 1, 1, 1])
+x = torch.tensor([0, 10, 20, 30, 40, 50, 60])
+
+# Get permuted indices
+permuted_indices = permute_within_batch(x, batch)
+
+# Use permuted indices to get the permuted tensor
+permuted_x = x[permuted_indices]
+
+print("Original x:", x)
+print("Permuted x:", permuted_x)
+print("Permuted indices:", permuted_indices)
 
 def timestep_embedding(timesteps, dim, max_period=10000):
     """
